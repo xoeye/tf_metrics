0f56bbd8a20a4495e1d9a2e26afaa93214290209
Matt Eby <meby@xoi.io>
2019-10-09 13:57:30 -0500

    remove package description

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ def has_gpu():
         return False
 
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
 
 
 install_requires = ["numpy"]
@@ -30,8 +30,8 @@ setup(
     version="0.0.1",
     author="Guillaume Genthial",
     description="Multi-class metrics for Tensorflow",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
     url="https://github.com",
     packages=find_packages(exclude=["test"]),
     classifiers=(
